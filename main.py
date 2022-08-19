ce6a33ea01a242aa0d22ae671ac7daf13cfdba56
Ayush Ajay <ayushajay52@gmail.com>
2022-08-19 08:05:12 +0530

    Update main.py
    changes

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import pdf_file
 def main_func(url , depth , ss , cluster):
 
     d = int(0)
-    # starting url. replace google with your own url.
+ 
     starting_url = str(url)
     ext = tldextract.extract(url)
     ss = int(ss)
@@ -30,28 +30,28 @@ def main_func(url , depth , ss , cluster):
     options.headless = True
 
 
-    #making new directory 
+ 
     get_mails.create_dir(ext.domain)
 
 
     f = open("Headers.txt" , "w+")
 
     
-    # a queue of urls to be crawled
+   
     unprocessed_urls = deque([starting_url])
 
-    # set of already crawled urls for email
+   
     processed_urls = set()
 
-    # a set of fetched emails
+   
     emails = set()
 
-    #list of image links fetched
+    
     images = []
-    # process urls one by one from unprocessed_url queue until queue is empty
+  
     while(len(unprocessed_urls) and d < int(depth)):
 
-        # move next url from the queue to the set of processed urls
+     
         url = unprocessed_urls.popleft()
         processed_urls.add(url)
 
@@ -61,25 +61,25 @@ def main_func(url , depth , ss , cluster):
                 ss -=1
             except:
                 pass
-        # extract base url to resolve relative links
+       
         parts = urlsplit(url)
         base_url = "{0.scheme}://{0.netloc}".format(parts)
         path = url[:url.rfind('/')+1] if '/' in parts.path else url
 
-        # get url's content
+      
         print("Crawling URL %s" % url)
         my_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36","Accept-Encoding": "gzip, deflate" ,
         "permissions-policy": "interest-cohort=()"}
         try:
             response = requests.get(url , headers=my_headers)
         except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
-            # ignore pages with errors and continue with next url
+          
             continue
 
         f.write(str(response.headers) + '\n')
-        # extract all email addresses and add them into the resulting set
+      
         emails.update(get_mails.gemails(response))
-        # create a beutiful soup for the html document
+        
         soup = BeautifulSoup(response.text, 'lxml')
 
         imgs = soup.find_all('img')
@@ -93,16 +93,16 @@ def main_func(url , depth , ss , cluster):
                     print(None)
                 images.append(a)
         
-        # Once this document is parsed and processed, now find and process all the anchors i.e. linked urls in this document
+      
         for anchor in soup.find_all("a"):
-            # extract link url from the anchor
+        
             link = anchor.attrs["href"] if "href" in anchor.attrs else ''
-            # resolve relative links (starting with /)
+            
             if link.startswith('/'):
                 link = base_url + link
             elif not link.startswith('http'):
                 link = path + link
-            # add the new url to the queue if it was not in unprocessed list nor in processed list yet
+            
             if not link in unprocessed_urls and not link in processed_urls:
                 unprocessed_urls.append(link)
         d+=1
